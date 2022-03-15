fba98a09589c43fe46f108d265bf580157307735
Kourosh Delsouz <kdel2008@gmail.com>
2022-03-15 16:49:27 -0700

    Added delete modals for inventory and warehouse.

---
+++ src/components/InventoryModal/InventoryModal.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./InventoryModal.scss";
+
+export default function InventoryModal() {
+
+    const modal = document.querySelector("#modal");
+
+    const span = document.querySelector(".close");
+
+    span.onclick = () => {
+        modal.style.display = "none";
+    };
+
+    return (
+        <section id="modal" class="modal">
+        <ul class="modal__list">
+            <li class="modal__list-close">
+                <span class="close">&times;</span>
+            </li>
+            <li class="modal__list-content">
+                <h2 class="modal__title">
+                    Delete VARIABLE inventory item?
+                </h2>
+                <p class="modal__text">
+                    Please confirm that you'd like to delete the VARIABLE from the inventory list. You won't be able to undo this action.  
+                </p>
+            </li>
+            <li class="modal__list-buttons">
+                <button class="button__delete">
+                    Delete
+                </button>
+                <button class="button__cancel">
+                    Cancel
+                </button>
+            </li>
+        </ul>
+    </section>
+    );
+}
--- src/components/WarehouseModal/WarehouseModal.jsx
+++ src/components/WarehouseModal/WarehouseModal.jsx
@@ -13,22 +13,27 @@ export default function WarehouseModal() {
 
     return (
         <section id="modal" class="modal">
-            <ul class="modal__list">
-                <li class="modal__list-close">
-                    <span class="close">&times;</span>
-                </li>
-                <li class="modal__list-content">
-                    <h2 class="modal__title">Delete VARIABLE warehouse?</h2>
-                    <p class="modal__text">
-                        Please confirm that you'd like to delete the VARIABLE from the list
-                        of warehouses. You won't be able to undo this action.
-                    </p>
-                </li>
-                <li class="modal__list-buttons">
-                    <button class="button__delete">Delete</button>
-                    <button class="button__cancel">Cancel</button>
-                </li>
-            </ul>
-        </section>
+        <ul class="modal__list">
+            <li class="modal__list-close">
+                <span class="close">&times;</span>
+            </li>
+            <li class="modal__list-content">
+                <h2 class="modal__title">
+                    Delete VARIABLE warehouse?
+                </h2>
+                <p class="modal__text">
+                    Please confirm that you'd like to delete the VARIABLE from the list of warehouses. You won't be able to undo this action.  
+                </p>
+            </li>
+            <li class="modal__list-buttons">
+                <button class="button__delete">
+                    Delete
+                </button>
+                <button class="button__cancel">
+                    Cancel
+                </button>
+            </li>
+        </ul>
+    </section>
     );
 }
