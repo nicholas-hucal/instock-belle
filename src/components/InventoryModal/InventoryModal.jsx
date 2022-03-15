a6baa3236c677526e66af886d35d92113a75f5fe
Kourosh Delsouz <kdel2008@gmail.com>
2022-03-15 16:57:16 -0700

    Adjusted indents to be consistent

--- src/components/InventoryModal/InventoryModal.jsx
+++ src/components/InventoryModal/InventoryModal.jsx
@@ -13,27 +13,27 @@ export default function InventoryModal() {
 
     return (
         <section id="modal" class="modal">
-        <ul class="modal__list">
-            <li class="modal__list-close">
-                <span class="close">&times;</span>
-            </li>
-            <li class="modal__list-content">
-                <h2 class="modal__title">
-                    Delete VARIABLE inventory item?
-                </h2>
-                <p class="modal__text">
-                    Please confirm that you'd like to delete the VARIABLE from the inventory list. You won't be able to undo this action.  
-                </p>
-            </li>
-            <li class="modal__list-buttons">
-                <button class="button__delete">
-                    Delete
-                </button>
-                <button class="button__cancel">
-                    Cancel
-                </button>
-            </li>
-        </ul>
-    </section>
+            <ul class="modal__list">
+                <li class="modal__list-close">
+                    <span class="close">&times;</span>
+                </li>
+                <li class="modal__list-content">
+                    <h2 class="modal__title">
+                        Delete VARIABLE inventory item?
+                    </h2>
+                    <p class="modal__text">
+                        Please confirm that you'd like to delete the VARIABLE from the inventory list. You won't be able to undo this action.
+                    </p>
+                </li>
+                <li class="modal__list-buttons">
+                    <button class="button__delete">
+                        Delete
+                    </button>
+                    <button class="button__cancel">
+                        Cancel
+                    </button>
+                </li>
+            </ul>
+        </section>
     );
 }
--- src/components/WarehouseModal/WarehouseModal.jsx
+++ src/components/WarehouseModal/WarehouseModal.jsx
@@ -2,38 +2,38 @@ import React from "react";
 import "./WarehouseModal.scss";
 
 export default function WarehouseModal() {
-
+  
     const modal = document.querySelector("#modal");
 
     const span = document.querySelector(".close");
 
     span.onclick = () => {
-        modal.style.display = "none";
+      modal.style.display = "none";
     };
 
     return (
         <section id="modal" class="modal">
-        <ul class="modal__list">
-            <li class="modal__list-close">
-                <span class="close">&times;</span>
-            </li>
-            <li class="modal__list-content">
-                <h2 class="modal__title">
+            <ul class="modal__list">
+                <li class="modal__list-close">
+                    <span class="close">&times;</span>
+                </li>
+                <li class="modal__list-content">
+                    <h2 class="modal__title">
                     Delete VARIABLE warehouse?
-                </h2>
-                <p class="modal__text">
-                    Please confirm that you'd like to delete the VARIABLE from the list of warehouses. You won't be able to undo this action.  
-                </p>
-            </li>
-            <li class="modal__list-buttons">
-                <button class="button__delete">
-                    Delete
-                </button>
-                <button class="button__cancel">
-                    Cancel
-                </button>
-            </li>
-        </ul>
-    </section>
+                    </h2>
+                    <p class="modal__text">
+                    Please confirm that you'd like to delete the VARIABLE from the list
+            of warehouses. You won't be able to undo this action.                    </p>
+                </li>
+                <li class="modal__list-buttons">
+                    <button class="button__delete">
+                        Delete
+                    </button>
+                    <button class="button__cancel">
+                        Cancel
+                    </button>
+                </li>
+            </ul>
+        </section>
     );
 }
