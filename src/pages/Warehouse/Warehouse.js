963019af4d746cd12768dae413e8314adbe4268f
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-03-16 16:19:44 -0700

    created the warehouse component header

--- src/pages/Warehouse/Warehouse.js
+++ src/pages/Warehouse/Warehouse.js
@@ -1,11 +1,24 @@
 import './Warehouse.scss';
+import React from 'react';
+import {Link} from "react-router-dom";
+import arrow from "../../assets/icons/arrow_back-24px.svg";
+import editPen from "../../assets/icons/edit-24px.svg";
 
-const Warehouse = () => {
+class Warehouse extends React.Component{
+  render(){
   return (
-    <section className='warehouse'>
-        Warehouse Page
-    </section>
-  )
+    <main className='warehouse'>
+      <section className="warehouse__header">
+        <Link to="/">
+          <img src={arrow} className="warehouse__header-icons" />
+        </Link>
+        <h1 className="warehouse__header-text">Washington</h1>
+        <Link to="/">
+          <img src={editPen} className="warehouse__header-icons" />
+        </Link>
+      </section>
+    </main>
+  )}
 }
 
 export default Warehouse
