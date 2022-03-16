1a720c9c8522ee7b09f940c5fd6348a1db95188e
Sara Costa-Maia <sara_cm88@hotmail.com>
2022-03-15 20:34:40 -0700

    page & component styling complete

---
+++ src/components/Button/Button.js
@@ -0,0 +1,14 @@
+/**
+ * Component: Search Box
+ * @props tbd
+ */
+
+function Button () {
+    return (
+        <>
+            <p>((Button placeholder))</p>
+        </>
+    );
+};
+  
+ export default Button;
--- src/pages/Warehouses/Warehouses.js
+++ src/pages/Warehouses/Warehouses.js
@@ -1,6 +1,8 @@
 import './Warehouses.scss';
 import axios from 'axios';
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
+import Button from "../../components/Button/Button";
+import SearchBox from "../../components/SearchBox/SearchBox";
 import { Component } from 'react';
 
 class Warehouses extends Component {
@@ -28,16 +30,16 @@ class Warehouses extends Component {
                 <div className='warehouses__nav'>
                     <h1>Warehouses</h1>
                     <div className='warehouses__form'>
-                        <p>search</p>
-                        <p>button</p>
+                        <SearchBox />
+                        <Button />
                     </div>
                 </div>
                 <div className="warehouses__headers">
-                    <p className="warehouses__header">Warehouse</p>
-                    <p className="warehouses__header">Address</p>
-                    <p className="warehouses__header">Contact Name</p>
-                    <p className="warehouses__header">Contact Information</p>
-                    <p className="warehouses__header">Actions</p>
+                    <h3 className="warehouses__header warehouses__header--short">Warehouse</h3>
+                    <h3 className="warehouses__header warehouses__header--long">Address</h3>
+                    <h3 className="warehouses__header warehouses__header--short">Contact Name</h3>
+                    <h3 className="warehouses__header warehouses__header--long">Contact Information</h3>
+                    <h3 className="warehouses__header warehouses__header--action">Actions</h3>
                 </div>
                 <div className='warehouses__list'>
                     <WarehouseList displayList={this.state.displayedWarehouses} />
