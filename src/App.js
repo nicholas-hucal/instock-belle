3bd6fe6ca3710d74a3cc73592d35a7720fbfd3af
Sara Costa-Maia <sara_cm88@hotmail.com>
2022-03-17 19:20:39 -0700

    add inventory form created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './components/Header/Header'
 import Warehouse from './pages/Warehouse/Warehouse.js';
 import WarehouseAdd from './pages/WarehouseAdd/WarehouseAdd.js';
 import WarehouseEdit from './pages/WarehouseEdit/WarehouseEdit.js';
+import InventoryAdd from './pages/InventoryAdd/InventoryAdd.js';
+import InventoryEdit from './pages/InventoryEdit/InventoryEdit.js';
 import Inventory from './pages/Inventory/Inventory.js';
 import NotFound from './pages/NotFound/NotFound.js';
 import Footer from './components/Footer/Footer'
@@ -16,10 +18,10 @@ function App() {
     <main className="main">
       <section  className="main__content">
       <Switch>
-        <Route path="/inventory" component={Inventory} />
-        <Route path="/inventory/:inventoryId" component={Inventory} />
+        <Route path="/inventory" exact component={Inventory} />
+        <Route path="/inventory/add" exact component={InventoryAdd} />
+        <Route path="/inventory/:inventoryId" component={InventoryEdit} />
         <Route path="/inventory/:inventoryId/edit" component={Inventory} />
-        <Route path="/inventory/add" component={Inventory} />
         <Route path="/add" component={WarehouseAdd}/>
         <Route path="/:warehouseId/edit" component={WarehouseEdit} />
         <Route path="/:warehouseId" component={Warehouse} />
